874020d9ec834f57bd9a70d536f348e4309c8a44
Raiga SASAYAMA <raiga0310brilio@gmail.com>
2023-09-20 11:29:53 +0900

    feat: クリア画面の実装 敵の当たり判定の実装

--- src/app/_components/Canvas.tsx
+++ src/app/_components/Canvas.tsx
@@ -2,9 +2,20 @@
 
 import { useRef } from "react";
 import * as BABYLON from "babylonjs";
-import { Button, AdvancedDynamicTexture } from "babylonjs-gui";
+import { Button, AdvancedDynamicTexture, Image } from "babylonjs-gui";
 import 'babylonjs-materials';
 import "./Canvas.css";
+
+type PLayer = {
+    mesh: BABYLON.Mesh;
+    hp: number;
+    score: number;
+}
+
+type Enemy = {
+    mesh: BABYLON.Mesh;
+    hp: number;
+};
 
 export default function Canvas() {
     const gameRef = useRef(null);
@@ -24,8 +35,8 @@ export default function Canvas() {
             const skyBoxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
             skyBoxMaterial.backFaceCulling = false;
 
-            skyBoxMaterial.refractionTexture = new BABYLON.CubeTexture("https://assets.babylonjs.com/textures/skybox", scene);
-            skyBoxMaterial.refractionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
+            skyBoxMaterial.reflectionTexture = new BABYLON.CubeTexture("https://assets.babylonjs.com/textures/skybox", scene);
+            skyBoxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
 
             skyBoxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
             skyBoxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
@@ -45,9 +56,14 @@ export default function Canvas() {
             ground.material = groundMat;
 
             const boxSize = 1;
-            const box = makeBox("box1", new BABYLON.Vector3(1, 1, 1), new BABYLON.Vector3(0, 0, -45));
+            const box = makeBox("box1", new BABYLON.Vector3(1, 1, 1), new BABYLON.Vector3(0, 0, -50));
+            const player: PLayer = {mesh: box, hp: 5, score: 0};
 
             box.position.addInPlaceFromFloats(0, boxSize / 2.0, 0);
+
+            const numIntEnemies = 5;
+            const enemyHp = 3;
+            let intEnemies: Enemy[] = makeEnemies(numIntEnemies, enemyHp);
 
             let isLeftPressed: boolean = false;
             let isRightPressed: boolean = false;
@@ -80,10 +96,14 @@ export default function Canvas() {
             let currentBulletCooldown = 0;
 
             // GUI
-            let appState: "start" | "play" | "pause" = "start";
+            let appState: "start" | "play" | "pause" | "" | "clear" = "start";
             let advancedTexture = AdvancedDynamicTexture.CreateFullscreenUI("UI");
             advancedTexture.background = "rgba(128, 128, 128, 0.7)"; // 透明度の高いグレー
 
+            const logo = new Image("image", "/Title.jpg");
+            logo.width = "800px";
+            logo.height = "185px";
+            logo.top = "-235px";
             const button1 = Button.CreateSimpleButton("but1", "Start");
             button1.width = "150px"
             button1.height = "40px";
@@ -93,6 +113,7 @@ export default function Canvas() {
             button1.onPointerUpObservable.add(function() {
                 appState = "play";
                 advancedTexture.removeControl(button1);
+                advancedTexture.removeControl(logo);
             });
             const button2 = Button.CreateSimpleButton("but2", "Resume");
             button2.width = "150px";
@@ -105,6 +126,7 @@ export default function Canvas() {
                 appState = "play";
                 advancedTexture.removeControl(button2);
                 advancedTexture.removeControl(button3);
+                advancedTexture.removeControl(logo);
             });
             const button3 = Button.CreateSimpleButton("but3", "Title");
             button3.width = "150px";
@@ -115,16 +137,28 @@ export default function Canvas() {
             button3.top = "90px"
             button3.onPointerUpObservable.add(function() {
                 appState = "start";
-                box.position.x = 0;
+                player.mesh.position.x = 0;
                 scene.meshes
                     .filter((mesh) => mesh.name === "bullet")
                     .map((mesh) => mesh.dispose());
+                intEnemies.map((enemy) => {
+                   enemy.mesh.position.z = 50;
+                });
 
                 bullets = [];
+                intEnemies.map((enemy) => enemy.mesh.dispose());
+                intEnemies = [];
+                intEnemies = makeEnemies(numIntEnemies, enemyHp);
                 advancedTexture.removeControl(button2);
                 advancedTexture.removeControl(button3);
-            })
+                advancedTexture.removeControl(congrats);
+            });
+            const congrats =new Image("congrats", "/Congrats.png");
+            congrats.width = "800px";
+            congrats.height = "284px";
+            congrats.top = "-334px";
             advancedTexture.addControl(button1);
+            advancedTexture.addControl(logo);
 
             scene.onKeyboardObservable.add((keyboard) => {
                 switch (keyboard.type) {
@@ -149,22 +183,61 @@ export default function Canvas() {
                     advancedTexture.background = "rgba(128, 128, 128, 0)"; // 透明度の高いグレー
 
                     const texture = groundMat.diffuseTexture as BABYLON.Texture;
+                    intEnemies.map((enemy) => {
+                        enemy.mesh.position.z -= 0.1;
+                    });
                     texture.vOffset += 0.00625;
+
                     if (isLeftPressed) {
-                        box.position.x -= animationSpeed;
+                        player.mesh.position.x -= animationSpeed;
                     } else if (isRightPressed) {
-                        box.position.x += animationSpeed;
+                        player.mesh.position.x += animationSpeed;
                     }
+
                 } else if (appState === "start") {
                     advancedTexture.background = "rgba(128, 128, 128, 0.7)"; // 透明度の高いグレー
                     advancedTexture.addControl(button1);
+                    advancedTexture.addControl(logo);
                 } else if (appState === "pause") {
                     advancedTexture.background = "rgba(128, 128, 128, 0.7)"; // 透明度の高いグレー
 
                     advancedTexture.addControl(button2);
                     advancedTexture.addControl(button3);
+                    advancedTexture.addControl(logo);
+                } else if(appState === "clear") {
+                    advancedTexture.background = "rgba(128, 128, 128, 0.7)";
+                    advancedTexture.addControl(button3);
+                    advancedTexture.addControl(congrats);
                 }
             }
+
+            scene.registerBeforeRender(function() {
+                if (appState === "play") {
+                    for(let i = 0; i < bullets.length; i++) {
+                        const bullet = bullets[i];
+
+                        for(let j = 0; j < intEnemies.length; j++) {
+                            const enemy = intEnemies[j];
+
+                            if (bullet.intersectsMesh(enemy.mesh, false)) {
+                                bullet.dispose();
+                                bullets.splice(i, 1);
+
+                                if (enemy.hp <= 0) {
+                                    enemy.mesh.dispose();
+                                    intEnemies.splice(j, 1);
+                                } else {
+                                    enemy.hp--;
+                                }
+                            }
+                        }
+                    }
+
+                    if (intEnemies.length === 0) {
+                        appState = "clear";
+                    }
+                }
+            });
 
             engine.runRenderLoop(() => {
                 scene.render();
@@ -212,4 +285,16 @@ function makeBox(name: string, scale: BABYLON.Vector3, position: BABYLON.Vector3
     box.position = position;
 
     return box;
+}
+
+function makeEnemies(num: number, hp: number): Enemy[] {
+    const enemies: Enemy[] = [];
+
+    for(let i = 0; i < num; i++) {
+        const enemyBox = makeBox("enemy", new BABYLON.Vector3(2, 2, 2), new BABYLON.Vector3(-(5 *(num - 1) / 2) + i * 5, 1, 60));
+
+        enemies.push({mesh: enemyBox, hp: hp});
+    }
+
+    return enemies;
 }
